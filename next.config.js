51cf77c6b911fc97d12af2e07a1d509b8d8bfe14
Travis <travis@TravisdeMac-mini.local>
2026-03-04 23:31:24 +0800

    fix: remove experimental.forceSwcTransforms

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    forceSwcTransforms: true,
-  },
   generateBuildId: async () => require('crypto').randomBytes(8).toString('hex'),
 }
 
